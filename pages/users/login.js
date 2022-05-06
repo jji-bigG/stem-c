decce16aac0b9c116daa16a6647327a4c2736eef
Jerry Ji <ruiyang-jerry@outlook.com>
2022-05-06 12:07:30 -0700

    authentication pages bootstrapping

---
+++ pages/users/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInSide from "../../components/Auth/SigninSide";
+
+const Login = () => {
+  return (
+    <div>
+      <SignInSide />
+    </div>
+  );
+};
+
+export default Login;
