0508c8d91508e253d8575f889dc2669b9e633607
Jerry Ji <ruiyang-jerry@outlook.com>
2022-05-06 19:03:44 -0700

    setting up redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import Default from "../layouts/Default";
+import { Provider } from "react-redux";
+import store from "../store";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
